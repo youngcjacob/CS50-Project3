c2107f3a6de5f4ad70d395f8bd68d93e752975cc
youngcjacob <72167849+youngcjacob@users.noreply.github.com>
2022-04-27 11:34:40 -0400

    Design Outline Complete

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
   load_mailbox('inbox')
 });
 
-function compose_email() {
+function compose_email(reply) {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
@@ -40,12 +40,68 @@ function load_mailbox(mailbox) {
   //elif mailbox = 'inbox'
       //display inbox 
   
-  //start with displaying all emails in the general inbox
+  //passes the desired mailbox to a function to get all the emails for that mailbox type
   get_emails(mailbox);
 }
 
-  // Code added as part of the project
+//_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+//ALL CODE BEYOND THIS POINT WAS WRITTEN BY ME
   
+//this function gets all the emails for the mailbox type and passes those emails to the correct function to display them
+//OPTIONAL - move this inside of load_mailbox function
+function get_emails(mailbox) {
+  const url = mailbox
+  fetch(`/emails/${url}`)
+  .then(response =>
+    response.json())
+
+  .then(emails => { 
+  console.log('resolved', emails);
+  emails.forEach(element => { document.querySelector('#emails-view').append(element.id);
+  document.querySelector('#emails-view').append(element.body);
+    });
+  });
+
+// for each email
+  // list the title - also have a hyperlink to the specific email 
+  // list the subject
+  // list the body  
+
+  return false;  
+}
+
+
+// function display_sent(emails) {}
+  //have a for loop displaying all sent emails 
+  //will have a hyperlink to bring user to specific email if clicking on the Id
+
+
+// function display_inbox(emails) {}
+  //have a for loop displaying all received emails 
+  //will have a hyperlink to bring user to specific email if clicking on the Id
+
+// function single_email_details(email_id) {}
+  //Will need to get the contents of single email and display them  
+  //Need to show: email's sender, recipients, subject, timestamp, body
+  //want to add an additional div to inbox.html for displaying the contents(see the hints section for help)
+  //will want to update the email to read once it has been clicked on. Use a put reques to do so  
+  // fetch('/emails/100')
+  // .then(response => response.json())
+  // .then(email => {
+  //     // Print email
+  //     console.log(email);
+
+  //     // ... do something else with email ...
+  // });
+
+
+// function archive_unarchive(email) {}
+  //will pass a single email id into this function and allow the email to be archived or unarchived
+  //when viewing a single email, the user should be able to select to archive an email or unarchive the email
+  //when the button is clicked, take the user to the email inbox
+
+
+//This function will send an email to the desired recipient  
 function send_email() {
   let recipient = document.querySelector('#compose-recipients').value;
   let subject = document.querySelector('#compose-subject').value; 
@@ -62,38 +118,16 @@ function send_email() {
   .then(response => response.json())
   .then(data => {
       load_mailbox('sent');});
-  // show the sent mailbox
-   return false;
+  // passes the function get_emails the value of 'sent', which will render the sent emails in the webpage
+    return false;
 
   }
 
 
-
-function get_emails(mailbox) {
-  const url = mailbox
-  fetch(`/emails/${url}`)
-  .then(response =>
-    response.json())
-
-  .then(emails => { 
-  console.log('resolved', emails)
-  });
-
-  
-
-  return false;  
+// function reply_email(email_id) {}
+  //need to add an optional parameter to send email
+  //will pass send_email a parameter which will then be used to populate recipient and subject 
 
 
-
-
-}
-
-
-// emails.forEach(element => { document.querySelector('#emails-view').append('test'); 
-  //   })
-   
-  // });
-  // for each email
-  // list the title - also have a hyperlink to the specific email 
-  // list the subject
-  // list the body
+//Tasks to be complete after watching User Interface video 
+  //Formatting color of read vs unread emails 
