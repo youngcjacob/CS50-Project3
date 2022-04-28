eb817f368038afaaadeb518b2d3faceefb2f43f1
youngcjacob <72167849+youngcjacob@users.noreply.github.com>
2022-04-28 14:16:11 -0400

    Archive and Read Functions Complete.

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  
 
   // send email function call
   document.querySelector('#compose-form').onsubmit = send_email;
@@ -14,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function compose_email(reply) {
-
+  
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -23,6 +24,11 @@ function compose_email(reply) {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+  if (reply.sender != null) {
+  document.querySelector('#compose-recipients').value = reply.sender;
+  document.querySelector('#compose-subject').value = 'Re: ' + reply.subject;
+  document.querySelector('#compose-body').value = 'On ' + reply.timestamp + " " + reply.sender + " wrote: " + reply.body;
+  }
 }
 
 function load_mailbox(mailbox) {
@@ -34,12 +40,6 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  // will need to insert if statements to direct to the correct mailbox depending on what the user is triyng to see
-  //if mailbox = 'sent'
-      //display sent mailbox 
-  //elif mailbox = 'inbox'
-      //display inbox 
-  
   //passes the desired mailbox to a function to get all the emails for that mailbox type
   get_emails(mailbox);
 }
@@ -47,14 +47,14 @@ function load_mailbox(mailbox) {
 //_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 //ALL CODE BEYOND THIS POINT WAS WRITTEN BY ME
   
-//this function gets all the emails for the mailbox type and passes those emails to the correct function to display them
+//This function gets all the emails for the mailbox type and passes those emails to the correct function to display them
 //OPTIONAL - move this inside of load_mailbox function
 function get_emails(mailbox) {
+
   const url = mailbox
   fetch(`/emails/${url}`)
   .then(response =>
     response.json())
-
   .then(emails => { 
     if (mailbox == 'sent') {
       display_sent(emails)
@@ -67,66 +67,128 @@ function get_emails(mailbox) {
     }
     console.log('resolved', emails);
   });
-
-// for each email
-  // list the title - also have a hyperlink to the specific email 
-  // list the subject
-  // list the body  
-
   return false;  
 }
 
-
+//This function displays all sent emails
 function display_sent(emails) {
-  //have a for loop displaying all sent emails 
-  //will have a hyperlink to bring user to specific email if clicking on the Id
-  
+
   emails.forEach(element => {
-    document.querySelector('#emails-view').append(element.id);
-    document.querySelector('#emails-view').append(element.body);
+    const add_div = document.createElement('div');
+    //insert if statement, if the email is read then class name should be 'read'
+    add_div.className = 'sent-email'
+    add_div.innerHTML = `Element ID: ${element.id} <br> Element Body: ${element.body} <br> Element Body: ${element.sender}`;
+    add_div.addEventListener('click', function() {single_email_details(element)});
+    //add_div.addEventListener('click', single_email_details(element)); -- currently auto-clicking. Not good
+    document.querySelector('#emails-view').append(add_div);
     });
-
-  // document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-
 }
-
+//this function displays all received emails and highlights the read emails in grey
 function display_inbox(emails) {
-  //have a for loop displaying all received emails 
-  //will have a hyperlink to bring user to specific email if clicking on the Id
+
   emails.forEach(element => {
-    document.querySelector('#emails-view').append(element.id);
-    document.querySelector('#emails-view').append(element.subject);
+    const add_div = document.createElement('div');
+    //insert if statement, if the email is read then class name should be 'read'
+    if (element.read === true) {
+      add_div.className = 'read-email'
+    }
+    else {
+      add_div.className = 'unread-email'
+    }
+    add_div.innerHTML = `<strong>${element.sender}</strong> ${element.subject} ${element.timestamp}`; //<span style="text-align:right;">TEXT</span>
+    add_div.addEventListener('click', function() {
+      single_email_details(element);
+      read(element);
+    });
+    //add_div.addEventListener('click', single_email_details(element)); -- currently auto-clicking. Not good
+    document.querySelector('#emails-view').append(add_div);
     });
   }
 
+//This function displays the archived emails
 function display_archived(emails) {
+
   //this will display only the emails that are archived
   emails.forEach(element => {
-    document.querySelector('#emails-view').append(element.id);
-    document.querySelector('#emails-view').append(element.sender);
-    });
+    if (element.archive = true) {
+      const add_div = document.createElement('div');
+      add_div.className = 'sent-email'
+      add_div.innerHTML = `Element ID: ${element.id} <br> Element Body: ${element.body}`;
+      add_div.addEventListener('click', function() {single_email_details(element)});
+      //add_div.addEventListener('click', single_email_details(element)); -- currently auto-clicking. Not good
+      document.querySelector('#emails-view').append(add_div);
+    }
+  });
+  return false;
 }
 
-// function single_email_details(email_id) {}
-  //Will need to get the contents of single email and display them  
-  //Need to show: email's sender, recipients, subject, timestamp, body
-  //want to add an additional div to inbox.html for displaying the contents(see the hints section for help)
-  //will want to update the email to read once it has been clicked on. Use a put reques to do so  
-  // fetch('/emails/100')
-  // .then(response => response.json())
-  // .then(email => {
-  //     // Print email
-  //     console.log(email);
+//this function displays details for a single email
+function single_email_details(email_id) {
 
-  //     // ... do something else with email ...
-  // });
+  document.querySelector('#emails-view').innerHTML = `<h3></h3>`; //resets the page title to blank 
+  const add_div = document.createElement('div');
+  add_div.className = 'sent-email' //**update this to change format of single email at later point**
+  //let user = document.getElementById('#user').value;
+  let archive_button = 'Archive?';
+  if (email_id.archived === true) {
+    archive_button = 'Unarchive?'
+  }
 
+  add_div.innerHTML = 
+    `<strong>From:</strong> ${email_id.sender} <br>
+    <strong>To:</strong> ${email_id.recipients} <br>
+    <strong>Subject:</strong> ${email_id.subject}  <br>
+    <strong>Timestamp:</strong> ${email_id.timestamp} <br>
+    <strong>Timestamp:</strong> ${email_id.email} <br>
+    <button class="btn btn-sm btn-outline-primary" id="reply">Reply</button>
+    <button class="btn btn-sm btn-outline-primary" id="archive">${archive_button}</button><br>
+    <hr>
+    ${email_id.body}
+    `;
+  
+  add_div.querySelector('#reply').addEventListener('click', function() {
+    compose_email(email_id)});
 
-// function archive_unarchive(email) {}
-  //will pass a single email id into this function and allow the email to be archived or unarchived
-  //when viewing a single email, the user should be able to select to archive an email or unarchive the email
-  //when the button is clicked, take the user to the email inbox
+  add_div.querySelector('#archive').addEventListener('click', function() {
+    archive_unarchive(email_id)})
+   
+  document.querySelector('#emails-view').append(add_div);
 
+  return false;
+}
+
+function archive_unarchive(email) {
+  let archived_status = email.archived;
+  if (email.archived == false) {
+    archived_status = true
+  }
+  else {
+    archived_status = false
+  }
+  const email_id = email.id
+  const status = {archived: archived_status}
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify(
+        status
+    )
+  })
+  .then(response => load_mailbox('inbox'))
+  return false;
+  
+}
+
+//this function is to update the email to read once it has been clicked
+function read(email) {
+  const email_id = email.id
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  })
+  return false;
+}
 
 //This function will send an email to the desired recipient  
 function send_email() {
@@ -148,14 +210,7 @@ function send_email() {
     });
   // passes the function get_emails the value of 'sent', which will render the sent emails in the webpage
     return false;
-
   }
 
-
-// function reply_email(email_id) {}
-  //need to add an optional parameter to send email
-  //will pass send_email a parameter which will then be used to populate recipient and subject 
-
-
-//Tasks to be complete after watching User Interface video 
-  //Formatting color of read vs unread emails 
+//Tasks to be complete________________________________________________________________________________________________________
+  //hide archive from sent mailbox
